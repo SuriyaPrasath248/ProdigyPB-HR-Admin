ca77b5fe97b0529e1a8a52190baae149687310a9
SuriyaPrasath248 <m.s.prasath5818@gmail.com>
2025-03-11 17:04:55 +0530

    firebase chnages

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore, doc, setDoc, updateDoc, arrayUnion, getDoc } from "fireba
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDOqL8rp-dCRhk1I2USJrx6q1HcVLS7CjA",
-    authDomain: "ragapplicaton.firebaseapp.com",
-    projectId: "ragapplicaton",
-    storageBucket: "ragapplicaton.appspot.com",
-    messagingSenderId: "191054595011",
-    appId: "1:191054595011:web:ceaca88f0d01beefef3376",
-    measurementId: "G-9VXYJ2L2WK"
-};
+    apiKey: "AIzaSyDrAWKqX1TWFykfTVEPwajLXPxjeRURadA",
+    authDomain: "prodigypb-28f3c.firebaseapp.com",
+    projectId: "prodigypb-28f3c",
+    storageBucket: "prodigypb-28f3c.firebasestorage.app",
+    messagingSenderId: "376436174826",
+    appId: "1:376436174826:web:7e202a0f2d9852534a0c93",
+    measurementId: "G-JV202163Y5"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
